6f8a52acfecebb58a852c6ae0b028b1115ec3aec
muna <muna.alharbi.90@gmail.com>
2021-12-07 05:03:40 +0300

    up

--- app/src/main/java/com/example/buttonapp1/MainActivity.kt
+++ app/src/main/java/com/example/buttonapp1/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.buttonapp1
+ package com.example.buttonapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myText = findViewById<TextView>(R.id.firstTV)
+        myText.text = "Home Sweet Home"
     }
 }
