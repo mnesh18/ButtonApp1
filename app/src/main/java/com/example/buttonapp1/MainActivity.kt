98950f8cab3674a135d2aded1348f5869e1d9d66
muna <muna.alharbi.90@gmail.com>
2021-12-07 05:11:18 +0300

    up

--- app/src/main/java/com/example/buttonapp1/MainActivity.kt
+++ app/src/main/java/com/example/buttonapp1/MainActivity.kt
@@ -5,11 +5,19 @@ import android.os.Bundle
 import android.widget.TextView
 
  class MainActivity : AppCompatActivity() {
+
+     lateinit var myText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var myText = findViewById<TextView>(R.id.firstTV)
         myText.text = "Home Sweet Home"
+        changText()
     }
+
+     fun changText(){
+         myText.text = "Back"
+     }
 }
